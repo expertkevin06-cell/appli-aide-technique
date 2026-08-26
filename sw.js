325073348c8700aede19c45bb43ffa9a430c3c6d
expertkevin06-cell <expertkevin06@gmail.com>
2026-08-26 15:06:00 +0200

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-// sw.js
-const CACHE='mrt-cache-v1';
+const CACHE='mrt-cache-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
  const url=new URL(e.request.url);
  if(url.origin!==location.origin)return;
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-   const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return res;
- }).catch(()=>caches.match('./index.html'))));
+ if(e.request.mode==='navigate'){
+  e.respondWith(fetch(e.request).then(res=>{const cl=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',cl));return res;}).catch(()=>caches.match('./index.html')));
+ }else{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return res;})));
+ }
 });
